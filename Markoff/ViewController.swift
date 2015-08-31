13ae721565fc4a97c2e3d279b7c8d6071e4374b8
Reda Lemeden <reda@thoughtbot.com>
2015-08-31 12:44:22 +0200

    Initial commit

---
+++ Markoff/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Markoff
+//
+//  Created by Reda Lemeden on 31/08/2015.
+//  Copyright © 2015 thoughtbot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
