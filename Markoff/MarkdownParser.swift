ee130b4cb11698b342ad329df7025c335c201598
Reda Lemeden <reda@thoughtbot.com>
2015-09-04 11:00:28 +0200

    Use temporary file to strip YAML front-matter

--- Markoff/MarkdownParser.swift
+++ Markoff/MarkdownParser.swift
@@ -1,21 +1,44 @@
 import Foundation
 
 class MarkdownParser: NSObject {
+  let tempFileURL = temporaryFileURL()
+
   private var executableURL: NSURL {
     return NSBundle.mainBundle().URLForResource("cmark", withExtension: nil)!
   }
 
   func parse(filePath: String, handler: String -> ()) {
+    writeTempFile(originalFilePath: filePath)
+
     let stdout = NSPipe()
     let task = try! NSUserUnixTask(URL: executableURL)
     task.standardOutput = stdout.fileHandleForWriting
 
-    task.executeWithArguments([filePath], completionHandler: { _ in
+    task.executeWithArguments([tempFileURL.path!], completionHandler: { _ in
       let outputData = stdout.fileHandleForReading.readDataToEndOfFile()
       let output = NSString(data: outputData, encoding: NSUTF8StringEncoding) as? String ?? "Parsing failed."
       handler(output)
     })
   }
+
+  private func writeTempFile(originalFilePath path: String) -> Bool {
+    guard let originalData = NSData(contentsOfFile: path),
+      let originalString = NSString(data: originalData, encoding: NSUTF8StringEncoding) as? String,
+      let sanitizedData = removeFrontMatter(originalString).dataUsingEncoding(NSUTF8StringEncoding) else { return false }
+
+    return sanitizedData.writeToURL(tempFileURL, atomically: false)
+  }
+
+  deinit {
+    _ = try? NSFileManager.defaultManager().removeItemAtURL(tempFileURL)
+  }
+}
+
+private func temporaryFileURL() -> NSURL {
+  let UUIDString = CFUUIDCreateString(nil, CFUUIDCreate(nil)) as String
+  let tempDirURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
+  let tempFileURL = tempDirURL.URLByAppendingPathComponent(UUIDString)
+  return tempFileURL
 }
 
 private func removeFrontMatter(markdown: String) -> String {
