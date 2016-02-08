404a2cb60bc7028aa549fd1acb5451a18a2b4b02
Reda Lemeden <git@kaishin.haz.email>
2016-02-08 11:11:40 +0100

    Show YAML front matter as code block

--- Markoff/MarkdownParser.swift
+++ Markoff/MarkdownParser.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 class MarkdownParser: NSObject {
-  let tempFileURL = temporaryFileURL()
+  // MARK: - Private Properties
+
+  private lazy var tempFileURL: NSURL = {
+    let UUIDString = CFUUIDCreateString(nil, CFUUIDCreate(nil)) as String
+    let tempDirURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
+    let fileURL = tempDirURL.URLByAppendingPathComponent(UUIDString)
+    return fileURL
+  }()
+
+  // MARK: - Public Methods
 
   func parse(filePath: String, handler: String -> ()) {
     writeTempFile(originalFilePath: filePath)
@@ -24,34 +33,34 @@ class MarkdownParser: NSObject {
     task.launch()
   }
 
-  private func writeTempFile(originalFilePath path: String) -> Bool {
-    guard let originalData = NSData(contentsOfFile: path),
-      let originalString = NSString(data: originalData, encoding: NSUTF8StringEncoding) as? String,
-      let sanitizedData = removeFrontMatter(originalString).dataUsingEncoding(NSUTF8StringEncoding) else { return false }
-
-    return sanitizedData.writeToURL(tempFileURL, atomically: false)
-  }
+  // MARK: - Lifecycle
 
   deinit {
     do {
       try NSFileManager.defaultManager().removeItemAtURL(tempFileURL)
     } catch { return }
   }
-}
 
-private func temporaryFileURL() -> NSURL {
-  let UUIDString = CFUUIDCreateString(nil, CFUUIDCreate(nil)) as String
-  let tempDirURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
-  let tempFileURL = tempDirURL.URLByAppendingPathComponent(UUIDString)
-  return tempFileURL
-}
+  // MARK: - Private Methods
 
-private func removeFrontMatter(markdown: String) -> String {
-  let result = markdown =~ "-{3}[a-z]*\n[\\s\\S]*?\n-{3}"
-  if result.isMatching {
-    let frontMatter = result.matches[0]
-    return markdown.stringByReplacingOccurrencesOfString(frontMatter, withString: "")
-  } else {
-    return markdown
+  private func writeTempFile(originalFilePath path: String) -> Bool {
+    guard let originalData = NSData(contentsOfFile: path),
+      let originalString = NSString(data: originalData, encoding: NSUTF8StringEncoding) as? String,
+      let sanitizedData = transformFrontMatter(originalString).dataUsingEncoding(NSUTF8StringEncoding) else { return false }
+
+    return sanitizedData.writeToURL(tempFileURL, atomically: false)
+  }
+
+  private func transformFrontMatter(markdown: String) -> String {
+    let result = markdown =~ "-{3}[a-z]*\n[\\s\\S]*?\n-{3}"
+    if result.isMatching {
+      let frontMatter = result.matches[0]
+      let codeBlockString = frontMatter.stringByReplacingOccurrencesOfString("---", withString: "~~~")
+      return markdown.stringByReplacingOccurrencesOfString(frontMatter, withString: codeBlockString)
+    } else {
+      return markdown
+    }
   }
 }
+
+
--- sample.md
+++ sample.md
@@ -1,7 +1,7 @@
 ---
 title: "Sample Markdown Document"
-tags: "tag-1", "tag-2", "tag-3"
-author: Reda Lemeden
+tags: tag-one, tag-two, tag-three
+author: "Reda Lemeden"
 ---
 
 # Chapter 1
