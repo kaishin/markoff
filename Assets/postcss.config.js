cfa89f8f84172bb90f2ee62e378177e12fd908e7
Reda Lemeden <git@redalemeden.com>
2020-06-26 17:20:44 +0200

    Update to use new SwiftUI life cycle on Big Sur

---
+++ Assets/postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')({
+      stage: 0,
+      features: {
+        'matches-pseudo-class': false,
+      },
+    }),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
+};
